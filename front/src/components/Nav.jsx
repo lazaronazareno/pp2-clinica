79500222a6a42bfc7d0d21e54283ef1d2944eeca
Lucascn21 <lucascn21@gmail.com>
2024-10-13 23:56:46 -0300

    techno bullshit is gone

--- front/src/components/Nav.jsx
+++ front/src/components/Nav.jsx
@@ -2,13 +2,11 @@ import { Link } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
-    const bla = new Audio("./bla.mp3");
-
   return (
     <nav>
       <ul>
         <li>
-          <Link onClick={bla.play()}to="/logout">logout 👻</Link>
+          <Link to="/logout">logout 👻</Link>
         </li>
       </ul>
     </nav>
