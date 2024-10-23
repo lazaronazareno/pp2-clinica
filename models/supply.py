7ba86e4bb24811ea023ae2c76eef8a161784e14f
Lucascn21 <lucascn21@gmail.com>
2024-10-22 23:01:59 -0300

    no miro 🙈

--- models/supply.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from db.database import base
-
-class Supply(base):
-    __tablename__ = 'insumo'
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    nombre = Column(String(45), nullable=False)
-    stock = Column(Integer, nullable=False)
