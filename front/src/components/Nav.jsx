92a301ff337ad5a8df6af0671a89935f664ba286
Lucascn21 <lucascn21@gmail.com>
2024-10-27 21:48:31 -0300

    nav fix

--- front/src/components/Nav.jsx
+++ front/src/components/Nav.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 const Nav = () => {
   const [cookies, setCookie] = useCookies(["user"]);
   const navigate = useNavigate();
+
   const handleLogout = () => {
     Object.keys(cookies).forEach((key) => {
       setCookie(key, "", { path: "/" });
@@ -15,15 +16,20 @@ const Nav = () => {
   return (
     <nav>
       <ul>
-        {cookies.user && (
+        {/* Verifica si cookies.user y cookies.user.id existen antes de mostrarlos */}
+        {cookies.user ? (
           <>
             <li>
-              <p>Bienvenido {cookies.user.name || cookies.user}</p>{" "}
+              <p>Bienvenido {cookies.user.name}</p>
             </li>
             <li>
               <button onClick={handleLogout}>logout 👻</button>
             </li>
           </>
+        ) : (
+          <li>
+            <p>No hay usuario logueado</p>
+          </li>
         )}
       </ul>
     </nav>
