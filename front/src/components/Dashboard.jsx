b3e73a1e329fd59bd44fcc31a562d5f5bb6d158c
Lucascn21 <lucascn21@gmail.com>
2024-11-02 09:21:50 -0300

    welp

--- front/src/components/Dashboard.jsx
+++ front/src/components/Dashboard.jsx
@@ -25,6 +25,12 @@ const Dashboard = () => {
     : "http://localhost";
 
   const [data, setData] = React.useState([]);
+  const [newRow, setNewRow] = React.useState(
+    DASHBOARD_HEADERS[section].reduce((acc, key) => {
+      acc[key] = key === "date_birth" ? "" : key === "boolean" ? false : "";
+      return acc;
+    }, {})
+  );
 
   const {
     isPending,
@@ -65,9 +71,53 @@ const Dashboard = () => {
     }
   };
 
+  const handleCreate = async (newData) => {
+    const url = `${apiUrl}/${DASHBOARD_ENDPOINTS[section]}`;
+    try {
+      const response = await axios.post(url, newData, {
+        headers: {
+          Authorization: `Bearer ${cookies.user}`,
+        },
+      });
+      setData((prevData) => [...prevData, response.data]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleNewRowChange = (value, key) => {
+    setNewRow((prevRow) => ({ ...prevRow, [key]: value }));
+  };
+
+  const handleNewRowSave = async () => {
+    await handleCreate(newRow);
+    setNewRow(
+      DASHBOARD_HEADERS[section].reduce((acc, key) => {
+        acc[key] = key === "date_birth" ? "" : key === "boolean" ? false : "";
+        return acc;
+      }, {})
+    );
+  };
+
+  const handleDelete = async (id) => {
+    const url = `${apiUrl}/${DASHBOARD_ENDPOINTS[section]}/${id}`;
+    try {
+      await axios.delete(url, {
+        headers: {
+          Authorization: `Bearer ${cookies.user}`,
+        },
+      });
+      setData((prevData) => prevData.filter((row) => row.id !== id));
+      alert(`Se elimino el registro con id: ${id}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const theme = useTheme({
     HeaderRow: `
       background-color: #eaf5fd;
+      
     `,
     Row: `
       &:nth-of-type(odd) {
@@ -78,6 +128,12 @@ const Dashboard = () => {
         background-color: #eaf5fd;
       }
     `,
+    Table: `
+    --data-table-library_grid-template-columns:${DASHBOARD_HEADERS[section]
+      .map(() => "1fr")
+      .join(" ")} 1fr;
+    --data-table-library_grid-template-rows: 1fr;
+  `,
   });
 
   if (isPending) {
@@ -91,19 +147,86 @@ const Dashboard = () => {
 
   return (
     <main>
-      <Table data={{ nodes: data }} theme={theme}>
+      <Table data={{ nodes: data }} layout={{ custom: true }} theme={theme}>
         {(tableList) => (
           <>
             <Header>
               <HeaderRow>
+                <HeaderCell>Actions</HeaderCell>
                 {DASHBOARD_HEADERS[section].map((header) => (
                   <HeaderCell key={header}>{header}</HeaderCell>
                 ))}
               </HeaderRow>
             </Header>
             <Body>
+              <Row id="saveRow" item={newRow}>
+                <Cell>
+                  <button id="addButton" onClick={handleNewRowSave}>
+                    Save
+                  </button>
+                </Cell>
+                {DASHBOARD_HEADERS[section].map((key) => (
+                  <Cell key={key}>
+                    {typeof newRow[key] === "boolean" ||
+                    key === "is_admin" ||
+                    key === "is_doctor" ||
+                    key === "active" ? (
+                      <input
+                        type="checkbox"
+                        checked={newRow[key]}
+                        onChange={(event) =>
+                          handleNewRowChange(event.target.checked, key)
+                        }
+                      />
+                    ) : key === "date_birth" || key === "date" ? (
+                      <input
+                        type="date"
+                        value={newRow[key]}
+                        onChange={(event) =>
+                          handleNewRowChange(event.target.value, key)
+                        }
+                      />
+                    ) : [
+                        "dni",
+                        "user_id",
+                        "department_id",
+                        "stock",
+                        "medical_record_id",
+                      ].includes(key) ? (
+                      <input
+                        type="number"
+                        value={newRow[key]}
+                        onChange={(event) =>
+                          handleNewRowChange(event.target.value, key)
+                        }
+                      />
+                    ) : key === "mail" ? (
+                      <input
+                        type="email"
+                        value={newRow[key]}
+                        onChange={(event) =>
+                          handleNewRowChange(event.target.value, key)
+                        }
+                      />
+                    ) : (
+                      <input
+                        type="text"
+                        value={newRow[key]}
+                        onChange={(event) =>
+                          handleNewRowChange(event.target.value, key)
+                        }
+                      />
+                    )}
+                  </Cell>
+                ))}
+              </Row>
               {tableList.map((item) => (
                 <Row key={item.id} item={item}>
+                  <Cell>
+                    <button onClick={() => handleDelete(item.id)}>
+                      Delete
+                    </button>
+                  </Cell>
                   {DASHBOARD_HEADERS[section].map((key) => (
                     <Cell key={key}>
                       {typeof item[key] === "boolean" ? (
@@ -126,13 +249,48 @@ const Dashboard = () => {
                       ) : key === "date_birth" ? (
                         <input
                           type="date"
-                          style={{
-                            width: "100%",
-                            border: "none",
-                            fontSize: "1rem",
-                            padding: 0,
-                            margin: 0,
+                          value={item[key]}
+                          onChange={(event) =>
+                            handleUpdate(event.target.value, item.id, key)
+                          }
+                          onBlur={async (event) => {
+                            event.target.style.width = "100%";
+                            const tableData = tableList.find(
+                              (row) => row.id === item.id
+                            );
+                            const { data } = await handleSave(tableData);
+                            alert(
+                              `${key}, ${data[key]} se actualizo a ${event.target.value}`
+                            );
                           }}
+                        />
+                      ) : [
+                          "dni",
+                          "user_id",
+                          "department_id",
+                          "stock",
+                          "medical_record_id",
+                        ].includes(key) ? (
+                        <input
+                          type="number"
+                          value={item[key]}
+                          onChange={(event) =>
+                            handleUpdate(event.target.value, item.id, key)
+                          }
+                          onBlur={async (event) => {
+                            event.target.style.width = "100%";
+                            const tableData = tableList.find(
+                              (row) => row.id === item.id
+                            );
+                            const { data } = await handleSave(tableData);
+                            alert(
+                              `${key}, ${data[key]} se actualizo a ${event.target.value}`
+                            );
+                          }}
+                        />
+                      ) : key === "mail" ? (
+                        <input
+                          type="email"
                           value={item[key]}
                           onChange={(event) =>
                             handleUpdate(event.target.value, item.id, key)
@@ -151,13 +309,6 @@ const Dashboard = () => {
                       ) : (
                         <input
                           type="text"
-                          style={{
-                            width: "100%",
-                            border: "none",
-                            fontSize: "1rem",
-                            padding: 0,
-                            margin: 0,
-                          }}
                           value={item[key]}
                           onChange={(event) =>
                             handleUpdate(event.target.value, item.id, key)
