bb91d9b2d47ff2eacb62804b87e52713e7daf886
Lucascn21 <lucascn21@gmail.com>
2024-11-02 18:36:51 -0300

    backending

--- backend/schemas/appointment_schema.py
+++ backend/schemas/appointment_schema.py
@@ -18,6 +18,7 @@ class AppointmentResponse(BaseModel):
 class AppointmentEdit(BaseModel):
     date: date
     active: bool
+    medical_record_id: int
 
     class Config:
         from_attributes = True
