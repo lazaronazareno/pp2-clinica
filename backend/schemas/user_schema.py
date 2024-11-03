e050d81ba29ce272025c888c55188e7e108c07d6
Micaela Edith <romeromicaelaedith@gmail.com>
2024-11-03 19:57:41 -0300

    user validations

--- backend/schemas/user_schema.py
+++ backend/schemas/user_schema.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from datetime import date
 from typing import Optional
+import re
 
 
 class UserBase(BaseModel):
@@ -13,7 +14,36 @@ class UserBase(BaseModel):
     date_birth: date
     is_admin: Optional[bool] = False
     is_doctor: Optional[bool] = False
-
+    
+    
+    @field_validator("dni")
+    def validate_dni(cls, value):
+        if value < 0:
+            raise ValueError("El DNI no puede ser negativo.")
+        if len(str(value)) != 8:
+            raise ValueError("El DNI debe tener exactamente 8 dígitos.")
+        return value
+    
+    @field_validator("name")
+    def validate_name(cls, value):
+        if not re.match(r"^[A-Za-z\s]+$", value):
+            raise ValueError("El nombre solo puede contener letras y espacios.")
+        return value
+    
+    @field_validator("lastname")
+    def validate_lastname(cls, value):
+        if not re.match(r"^[A-Za-z\s]+$", value):
+            raise ValueError("El apellido solo puede contener letras y espacios.")
+        return value
+    
+    @field_validator("phone")
+    def validate_phone(cls, value):
+        if value is not None:
+            if not value.isdigit():
+                raise ValueError("El teléfono solo puede contener números.")
+            if int(value) < 0:
+                raise ValueError("El teléfono no puede ser un número negativo.")
+        return value
 
 class UserCreate(UserBase):
     pass
